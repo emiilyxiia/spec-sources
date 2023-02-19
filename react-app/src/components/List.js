4a933c1d9720f6a516dc93f333bacefa9e2aa91a
WilliamFreedman <freedmanw@bxscience.edu>
2023-02-19 13:12:54 -0500

    split sources into components

--- react-app/src/components/List.js
+++ react-app/src/components/List.js
@@ -1,26 +1,13 @@
+import Source from "./Source"
+
 function List()
 {
+    let num_entries = 0;
     return(
         <div id = "source_div">
-        <div className = "source">
-          <span className = "number">1</span>
-          <span className = "source_name">Presbo</span>
-          <span className = "source_email">presbo@columbia.edu</span>
-          <button className = "button" id = "delete" type="button">DELETE</button>
-        </div>
-        <div className = "source">
-          <span className = "number">2</span>
-          <span className = "source_name">John Jay Mouse</span>
-          <span className = "source_email">mouse@columbia.edu</span>
-          <button className = "button" id = "delete" type="button">DELETE</button>
-        </div>
-        <div className = "source">
-          <span className = "number">1</span>
-          <span className = "source_name">Water Bottle Man</span>
-          <span className = "source_email">flipper@columbia.edu</span>
-          <button className = "button" id = "delete" type="button">DELETE</button>
-        </div>
-
+          <Source num = {++num_entries} source_info = {{name: "Presbo", email: "presbo@columbia.edu"}}/>
+          <Source num = {++num_entries} source_info = {{name: "John Jay Mouse", email: "mouse@columbia.edu"}}/>
+          <Source num = {++num_entries} source_info = {{name: "Water Bottle Man", email: "flipper@columbia.edu"}}/>
       </div>
     );
 }
