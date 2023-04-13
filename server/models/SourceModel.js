174b5712277087a728c06ba995f1ad3f4e4ba131
Emily X <122341479+emiilyxiia@users.noreply.github.com>
2023-04-12 20:13:39 -0400

    connected to mongoDB database

--- server/models/SourceModel.js
+++ server/models/SourceModel.js
@@ -3,6 +3,12 @@ const Schema = mongoose.Schema;
 
 // WRITE THE SCHEMA HERE
 
-const sources = mongoose.model('spec_sources', YOUR_SCHEMA);
+const sourceSchema = new Schema({
+  id: { type: String, required: true},
+  name: { type: String, required: true},
+  email: { type: String, required: true}
+})
+
+const sources = mongoose.model('spec_sources', sourceSchema);
 
 module.exports = sources;
--- server/models/config.js
+++ server/models/config.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
+//doesn't push password to the github
 const dbUser = process.env.DBUSER;
 const dbName = process.env.DBNAME;
 const dbPassword = process.env.DBPASSWORD;
 const discardAfterXDays = 14;
-const URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.tp9ny.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@spec-sources.5dzaobh.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 module.exports = {
     URI: URI,
